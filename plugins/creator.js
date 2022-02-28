2312aacca82734bcd73e5191516491b7a91cbcb4
jetushack1 <89070690+jetushack1@users.noreply.github.com>
2022-02-28 16:57:31 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '918602239106', 'OLDUSER', m)
+  this.sendContact(m.chat, '%name', m)
 }
 handler.help = ['owner/creator']
 handler.tags = ['info']
